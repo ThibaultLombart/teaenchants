e9e8476835e3e9176c4bc582f0950380e16ce269
Lombart Thibault <lombart.thibault@gmail.com>
2024-09-06 23:55:46 +0200

    First push

---
+++ src/main/java/com/thibault_lombart/teaEnchants/TeaEnchants.java
@@ -0,0 +1,17 @@
+package com.thibault_lombart.teaEnchants;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TeaEnchants extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
